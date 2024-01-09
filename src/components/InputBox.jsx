8db479d9bffdf357052ccbd0b5966f668d45d0b0
sachinmaurya96 <125910801+sachinmaurya96@users.noreply.github.com>
2024-01-09 18:02:08 +0530

    currency converter app using vite react and tailwind css

---
+++ src/components/InputBox.jsx
@@ -0,0 +1,52 @@
+import React, { useId } from "react";
+
+const InputBox = ({
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOption = [],
+  selectedCurrency = "usd",
+  amountDisable = false,
+  currencyDisabled = false,
+  className = "",
+}) => {
+    const id = useId()
+  return (
+    <div className={`${className} bg-white p-3 rounded text-sm flex`}>
+      <div className="w-1/2 ">
+        <label htmlFor={id} className="text-black/40 mb-2 inline-block">{label}</label>
+        <input
+        id={id}
+          type="number"
+          className="outline-none w-full bg-transparent py-1.5"
+          placeholder="Amount"
+          value={amount}
+          disabled={amountDisable}
+          onChange={(e) =>
+            onAmountChange && onAmountChange(Number(e.target.value))
+          }
+        />
+      </div>
+      <div className="w-1/2 flex flex-wrap justify-end text-right">
+        <p className="text-black/40 mb-2 w-full">Corrency type</p>
+        <select
+          className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+          value={selectedCurrency}
+          onChange={(e) => {
+            onCurrencyChange && onCurrencyChange(e.target.value);
+          }}
+          disabled={currencyDisabled}
+        >
+          {currencyOption.map((currency) => (
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default InputBox;
